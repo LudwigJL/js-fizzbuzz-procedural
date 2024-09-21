206e58d443b70d31c4952b2f16b75dff327053b9
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-09-21 18:56:55 +0200

    done

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,22 @@ const answer = []
 
 // Write your code below this line
 
+for(let i = 1; i <= 15; i++){
+    answer.push(i)
+}
+
+for(let i = 0; i < answer.length; i++){
+    if(answer[i] % 3 === 0 && answer[i] % 5 === 0){
+        answer[i] = 'FizzBuzz'
+    }
+    else if(answer[i] % 3 === 0){
+        answer[i] = 'Fizz'
+    }
+    else if (answer[i] % 5 === 0){
+        answer[i] = 'Buzz'
+    }
+
+}
 
 
 
